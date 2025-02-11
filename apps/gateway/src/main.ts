6729f4f180cc8f56cef2eab83fa777021c102a3a
hardope <adeyeriopeoluwa05@gmail.com>
2025-02-11 22:10:53 +0100

    hotfix

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -6,6 +6,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
+	app.enableCors({
+		origin: true,
+	});
+
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
 
@@ -18,10 +22,6 @@ async function bootstrap() {
 
 	const docs = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('docs', app, docs);
-
-	app.enableCors({
-		origin: true,
-	});
 
 	await app.listen(process.env.PORT ?? 3000);
 }
