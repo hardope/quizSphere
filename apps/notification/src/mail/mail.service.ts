bd3e3becff10afef62399c069fa781470e22a928
hardope <adeyeriopeoluwa05@gmail.com>
2025-02-20 19:58:19 +0100

    hotfix verification email

--- apps/notification/src/mail/mail.service.ts
+++ apps/notification/src/mail/mail.service.ts
@@ -30,16 +30,16 @@ export class MailService {
 
         const verificationLink = `${process.env.FRONTEND_HOST}/verify-account/${user.email}/${verification.token}`;
 
-        const subject = 'Welcome to ClickViral';
-        const text = `Hello ${user.firstName},\n\nWelcome to ClickViral. Please click on the link below to verify your account:\n\n${verificationLink}\n\nIf you did not create an account, no further action is required.\nThis Link will expire in 5 minutes\n\nBest regards,\nThe ClickViral Team`;
+        const subject = 'Welcome to Sphere';
+        const text = `Hello ${user.firstName},\n\nWelcome to Sphere. Please click on the link below to verify your account:\n\n${verificationLink}\n\nIf you did not create an account, no further action is required.\nThis Link will expire in 5 minutes\n\nBest regards,\nThe Sphere Team`;
         const html = `
             <div style="font-family: Arial, sans-serif; line-height: 1.6;">
             <b>Hello ${user.firstName},</b>
-            <p>Welcome to ClickViral. Please click on the button below to verify your account:</p>
+            <p>Welcome to Sphere. Please click on the button below to verify your account:</p>
             <a href="${verificationLink}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Verify Account</a>
             <p>If you did not create an account, no further action is required.</p>
             <p>This link will expire in 5 minutes.</p>
-            <p>Best regards,<br/><b>The ClickViral Team</b></p>
+            <p>Best regards,<br/><b>The Sphere Team</b></p>
             </div>`;
 
         this.sendMail(user.email, subject, text, html);
@@ -67,7 +67,7 @@ export class MailService {
         const verificationLink = `http://localhost/reset-password/${verification.token}`;
 
         const subject = 'Password Reset Request';
-        const text = `Hello ${user.firstName},\n\nYou have requested a password reset. Please click on the link below to reset your password:\n\n${verificationLink}\n\nIf you did not request a password reset, no further action is required.\nThis Link will expire in 5 minutes\n\nBest regards,\nThe ClickViral Team`;
+        const text = `Hello ${user.firstName},\n\nYou have requested a password reset. Please click on the link below to reset your password:\n\n${verificationLink}\n\nIf you did not request a password reset, no further action is required.\nThis Link will expire in 5 minutes\n\nBest regards,\nThe Sphere Team`;
         const html = `
             <div style="font-family: Arial, sans-serif; line-height: 1.6;">
             <b>Hello ${user.firstName},</b>
@@ -75,7 +75,7 @@ export class MailService {
             <a href="${verificationLink}" style="display: inline-block; padding: 10px 20px; font-size: 16px; color: #fff; background-color: #007bff; text-decoration: none; border-radius: 5px;">Reset Password</a>
             <p>If you did not request a password reset, no further action is required.</p>
             <p>This link will expire in 5 minutes.</p>
-            <p>Best regards,<br/><b>The ClickViral Team</b></p>
+            <p>Best regards,<br/><b>The Sphere Team</b></p>
             </div>`;
 
         this.sendMail(user.email, subject, text, html);
@@ -84,7 +84,7 @@ export class MailService {
     async sendMail(to: string, subject: string, text: string, html: string) {
         try {
             await this.transporter.sendMail({
-                from: 'clickviralng@gmail.com',
+                from: 'Sphereng@gmail.com',
                 to,
                 subject,
                 text,
